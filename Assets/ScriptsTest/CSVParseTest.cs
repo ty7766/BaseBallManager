b5f9048a56c932a31aad0dde6bd2e0837dd85326
hyojoon Lim <hyojoon42@naver.com>
2026-06-28 15:22:54 +0900

    chore : CSV 파싱이 잘 되는지 테스트

---
+++ Assets/ScriptsTest/CSVParseTest.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class CSVParseTest : MonoBehaviour
+{
+    private CardCSVLoader cardCSVLoader;
+
+    private void Start()
+    {
+        cardCSVLoader = new CardCSVLoader();
+        var hitters = cardCSVLoader.LoadHitters();
+        var pitchers = cardCSVLoader.LoadPitchers();
+        Debug.Log($"Å¸ÀÚ : {hitters[0].Name}, OVR: {hitters[0].CalculateOVR()}, ÆÀ: {hitters[0].TeamName}");
+
+        Debug.Log($"Åõ¼ö : {pitchers[0].Name}, OVR: {pitchers[0].CalculateOVR()}, ÆÀ: {pitchers[0].TeamName}");
+    }
+}
