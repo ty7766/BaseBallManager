6a69560c2e323d08f9fd2d0a3c6ebee7befcd006
hyojoon Lim <hyojoon42@naver.com>
2026-07-11 13:51:29 +0900

    feature : TrainManager, 훈련 레벨, 비용 설정 완료 및 구현

--- Assets/Scripts/Cards/CardInstance.cs
+++ Assets/Scripts/Cards/CardInstance.cs
@@ -1,12 +1,16 @@
+using System.Collections.Generic;
+
 public class CardInstance
 {
     public int InstanceId { get; private set; }             //인스턴스ID
     public int CardId { get; private set; }                  //카드 ID - CSV와 연결
     public int EnhanceLevel { get; private set; }           //강화 레벨
     public int TrainLevel { get; private set; }              //훈련 레벨
-    public int[] TrainDelta { get; private set; }            //훈련 스탯 분배값
     public bool IsLocked { get; private set; }               //잠금 상태
     public bool BreakthroughUsed { get; private set; }       //훈련 돌파 사용 여부
+
+    private int[] _trainDelta;
+    public IReadOnlyList<int> TrainDelta => _trainDelta;     //훈련 스탯 분배값
 
     //카드를 처음 획득했을 때의 초기값 생성자
     public CardInstance(int instanceId, int cardId)
@@ -15,7 +19,7 @@ public class CardInstance
         CardId = cardId;
         EnhanceLevel = 0;
         TrainLevel = 1;
-        TrainDelta = new int[4];
+        _trainDelta = new int[4];
         IsLocked = false;
         BreakthroughUsed = false;
     }
@@ -40,7 +44,12 @@ public class CardInstance
     //delta : 이번 레벨 업에서 오른 각 스탯 증가량
     public void ApplyTrain(int[] delta)
     {
-     
+        for (int i = 0;  i < _trainDelta.Length; i++)
+        {
+            _trainDelta[i] += delta[i];
+        }
+
+        TrainLevel++;
     }
 
     //돌파 완료 표시
